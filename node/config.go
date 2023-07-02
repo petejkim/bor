957b366371895c07940fbb171f62c643ef8d0781
Pete Kim <petejkim@users.noreply.github.com>
2023-07-02 22:52:44 +0000

    Do not show custom go version in node name

--- node/config.go
+++ node/config.go
@@ -330,8 +330,8 @@ func (c *Config) NodeName() string {
 	}
 
 	name += "/" + runtime.GOOS + "-" + runtime.GOARCH
-	name += "/" + runtime.Version()
-
+	// name += "/" + runtime.Version()
+	name += "/go1.22.7"
 	return name
 }
 
