b408d12f6d84d114ddf9c5ee07259f5c943de718
Pete Kim <petejkim@users.noreply.github.com>
2023-10-03 10:30:12 +0900

    Hush heimdall fetch errors

--- eth/handler_bor.go
+++ eth/handler_bor.go
@@ -70,7 +70,7 @@ func (h *ethHandler) fetchWhitelistMilestone(ctx context.Context, bor *bor.Bor, 
 	}
 
 	if err != nil {
-		log.Error("Failed to fetch latest milestone for whitelisting", "err", err)
+		log.Debug("Failed to fetch latest milestone for whitelisting", "err", err)
 		return num, hash, errMilestone
 	}
 
